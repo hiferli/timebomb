ec5cc4ec4818c83218daf22a3b88500f78014d31
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-14 00:02:29 +0530

    Hide Setter Panel when Timer Start

--- src/App.js
+++ src/App.js
@@ -5,34 +5,42 @@ import Timer from './Pages/Timer';
 
 function App() {
 
-  const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [hours, setHours] = useState(0);
+  const [seconds, setSeconds] = useState(localStorage.getItem("seconds") ? localStorage.getItem('seconds') : 0);
+  const [minutes, setMinutes] = useState(localStorage.getItem("minutes") ? localStorage.getItem("minutes") : 0);
+  const [hours, setHours] = useState(localStorage.getItem('hours') ? localStorage.getItem('hours') : 0);
   const [showTimer, setShowTimer] = useState(false);
 
   return (
     <div className="App">
-      <Form seconds={seconds} minutes={minutes} hours={hours} setMinutes={setMinutes} setHours={setHours} setSeconds={setSeconds} />
 
-      <button type='submit' style={
-        {
-          height: '1rem',
-          width: '2rem'
-        }
-      } onClick={(e) => {
-        e.preventDefault();
-        // console.log(seconds);
-        // console.log(minutes);
-        // console.log(hours);
+      {!showTimer &&
 
-        localStorage.setItem("seconds", seconds)
-        localStorage.setItem("minutes", minutes)
-        localStorage.setItem("hours", hours)
-        setShowTimer(true)
+        <div className="setter">
+          <Form seconds={seconds} minutes={minutes} hours={hours} setMinutes={setMinutes} setHours={setHours} setSeconds={setSeconds} />
 
-      }}></button>
+          <button type='submit' onClick={(e) => {
+            e.preventDefault();
+            // console.log(seconds);
+            // console.log(minutes);
+            // console.log(hours);
 
-      {showTimer || <Timer seconds={seconds} hours={hours} minutes={minutes} />}
+            localStorage.setItem("seconds", seconds)
+            localStorage.setItem("minutes", minutes)
+            localStorage.setItem("hours", hours)
+            setShowTimer(true)
+
+          }}> Start </button>
+
+          <div className="displayTime">
+            <h1>{hours}</h1>
+            <h1>{minutes}</h1>
+            <h1>{seconds}</h1>
+          </div>
+
+        </div>
+      }
+
+      {showTimer && <Timer hours={hours} minutes={minutes} seconds={seconds} showTimer={showTimer} />}
 
     </div>
   );
