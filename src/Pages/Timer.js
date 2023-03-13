b6c28135eb09879368e47e63696b02aea5301062
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-14 00:35:59 +0530

    Showing Timer till time allows

--- src/Pages/Timer.js
+++ src/Pages/Timer.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Timer(props) {
+  var totalTime = 0;
+  const totalTimeCalculator = () => {
+    // console.log(((props.hours * 60 * 60) + (props.minutes * 60) + (props.seconds)) * 1000)
+    return ((props.hours * 60 * 60) + (props.minutes * 60) + (props.seconds) * 1000);
+  }
+
+  useEffect(() => {
+    totalTime = totalTimeCalculator();
+
+    setTimeout(() => {
+      props.setShowTimer(false);
+    }, totalTime)
+  }, []);
+
   return (
     <div>
       <h1>{props.hours}</h1>
       <h1>{props.minutes}</h1>
       <h1>{props.seconds}</h1>
-
-      {
-        setTimeout(() => {
-          props.setShowTimer(false);
-        }, 5000)
-      }
       
     </div>
   )
