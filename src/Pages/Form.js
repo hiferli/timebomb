4c086d736f18157a5f342aadc153288d5384a416
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-13 22:36:15 +0530

    Creating Basic Form

--- src/Pages/Form.js
+++ src/Pages/Form.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Form() {
-  return (
-    <div>
-      Hello Form
-    </div>
-  )
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [hours, setHours] = useState(0);
+    return (
+        <div>
+            <h1>Hello Form</h1>
+
+            <form action="">
+                
+                <label htmlFor="hours">Hours</label>
+                <br />
+                <input onChange={(event) => setHours(
+                    (event.target.value === '' ? 0 : parseInt(event.target.value))
+                    )} type="number" id="hours" name="hours" />
+                <br />
+                
+                <label htmlFor="minutes">Minutes</label>
+                <br />
+                <input onChange={(event) => setMinutes(
+                    (event.target.value === '' ? 0 : parseInt(event.target.value))
+                    )} type="number" id="minutes" name="minutes" />
+                <br />
+
+                <label htmlFor="seconds">Seconds</label>
+                <br />
+                <input onChange={(event) => setSeconds(
+                    (event.target.value === '' ? 0 : parseInt(event.target.value))
+                )} type="number" id="seconds" name="seconds" />
+                <br />
+
+                <button type='submit' onClick={(e) => {
+                    e.preventDefault();
+                    console.log(seconds);
+                    console.log(minutes);
+                    console.log(hours);
+                }}></button>
+            </form>
+
+        </div>
+    )
 }
