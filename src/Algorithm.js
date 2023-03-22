f94b4d9f74e231f7bebc19cc28cba0bdba1442e9
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-22 19:24:25 +0530

    Creating Class for Timer Algorithm

--- src/Algorithm.js
+++ src/Algorithm.js
@@ -1,45 +1,50 @@
-let hours = 0;
-let minutes = 0;
-let seconds = 5;
+class Timer {
+    constructor(hours , minutes , seconds){
+        this.hours = hours;    
+        this.minutes = minutes;
+        this.seconds = seconds;
+        this.convertTime();
+    }
 
-const convertTime = () => {
-    // Seconds to Minutes
-    minutes += parseInt(seconds / 60);
-    seconds = (seconds % 60);
-
-    // Minutes to Hours
-    hours += parseInt(minutes / 60);
-    minutes = (minutes % 60);
-}
-
-const totalTime = () => {
-    return (((hours * 60 * 60) + (minutes * 60) + (seconds)) * 1000);
-}
-
-
-const timer = setInterval(() => {
-    if (seconds === 0 && minutes === 0 && hours === 0) {
-        clearInterval(timer);
+    getTime(){
+        console.log(this.hours + " " + this.minutes + " " + this.seconds);
     }
     
-    if (minutes === 0) {
-        if (hours) {
-            hours -= 1;
-            minutes += 60;
-        }
-    }
-
-    if (seconds == 1) {
-        if (minutes) {
-            minutes -= 1;
-            seconds += 60;
-        }
-    }
-
-    console.log(hours + " " + minutes + " " + seconds);
-    seconds -= 1;
+    convertTime = () => {
+        // Seconds to Minutes
+        this.minutes += parseInt(this.seconds / 60);
+        this.seconds = (this.seconds % 60);
     
-}, 1000);
+        // Minutes to Hours
+        this.hours += parseInt(this.minutes / 60);
+        this.minutes = (this.minutes % 60);
+    }
+    
+    timer = setInterval(() => {
+        if (this.seconds === 0 && this.minutes === 0 && this.hours === 0) {
+            clearInterval(this.timer);
+        }
+        
+        if (this.minutes === -1) {
+            if (this.hours) {
+                this.hours -= 1;
+                this.minutes += 60;
+            }
+        }
+    
+        if (this.seconds == -1) {
+            if (this.minutes) {
+                this.minutes -= 1;
+                this.seconds += 60;
+            }
+        }
+    
+        console.log(this.hours + " " + this.minutes + " " + this.seconds);
+        this.seconds -= 1;
+        
+    }, 1000);
+}
 
+const time = new Timer(0 , 1 , 10);
+time.timer;
 
-console.log("Hlow")
