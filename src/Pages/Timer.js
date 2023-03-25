d2adfb3df97e13fd3b94c6f6efe0fc000725a3eb
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-25 18:15:08 +0530

    Timer Exept Stopper

--- src/Pages/Timer.js
+++ src/Pages/Timer.js
@@ -6,12 +6,48 @@ export default function Timer(props) {
 	const [hours, setHours] = useState(parseInt(props.hours));
 
 	const convertTime = () => {
+		console.log('object')
 		setMinutes(minutes + parseInt(seconds / 60));
 		setSeconds(seconds % 60);
 
 		setHours(hours + parseInt(minutes / 60));
 		setMinutes(minutes % 60);
     }
+
+	const countDown = () => setInterval(timeChange  , 1000);
+
+	function timeChange() {
+		if (seconds === 0 && minutes === 0 && hours === 0) {
+			props.setShowTimer(false);
+            clearInterval(countDown);
+        }
+        
+        if (minutes === -1) {
+            if (hours) {
+                // hours -= 1;
+				setHours(prevHours => prevHours - 1);
+                // minutes += 60;
+				setMinutes(prevMinutes => prevMinutes + 60);
+            }
+        }
+		
+        if (seconds == -1) {
+			if (minutes) {
+				// minutes -= 1;
+				setMinutes(prevMinutes => prevMinutes - 1);
+                // seconds += 60;
+				setSeconds(prevSeconds => prevSeconds + 60);
+            }
+        }
+    
+		setSeconds(prevSeconds => prevSeconds - 1);
+	}
+
+
+	useEffect(() => {
+		convertTime();
+		countDown();
+	}, []);
 
 	return (
 		<div>
